dce1e6f0a9aabeb880759cd7d548adcac78fa6a5
mlal123 <mlal123@live.unc.edu>
2017-02-28 21:57:11 -0500

    Update sight_type.js

--- sight_type.js
+++ sight_type.js
@@ -2,11 +2,12 @@
 /*global $, jQuery*/
 /*global alert */
 //variables
-var words, key, message, currentIndex, lesson = ["Please Select a Lesson"]
-    , lesson0, lesson1, lesson2, lesson3, lesson4, lesson5, lesson6, lesson7, lesson8, lesson9, letterIndex, newWord, input, stringlength, letter, lessonsize, gameOverSound = document.getElementById("gameoverAudio")
+var words, key, message, lessonIndex, lesson = ["Please Select a Lesson"]
+    , lesson0, lesson1, lesson2, lesson3, lesson4, lesson5, lesson6, lesson7, lesson8, lesson9, letterIndex, newWord, input, stringlength, letter, lessonsize, gameOverSound = document.createElement('audio')
     , is_it_finished;
+gameOverSound.src = "https://www.cs.unc.edu/~gb/uploaded-files/mlal123@CS.UNC.EDU/76376__spazzo-1493__game-over.wav";
 lessonsize = 0;
-currentIndex = 0;
+lessonIndex = 0;
 letterIndex = 0;
 stringlength = 0;
 is_it_finished = false;
@@ -23,16 +24,20 @@ lesson9 = ["fgjh", "fgjh", "fgjh"];
 
 function displayWord(word) {
     'use strict';
-    $("#messagebox").html(word[currentIndex]);
-    responsiveVoice.speak(word[currentIndex], "UK English Male", {
+    window.console.log("string length is " + stringlength + " and lesson size is " + lessonsize);
+    window.console.log("displayWord is " + word[lessonIndex] + " and lesson Index is " + lessonIndex + ", letter index is " + letterIndex);
+    window.console.log(word);
+    responsiveVoice.speak(word[lessonIndex], "UK English Male", {
         rate: 1.0
     });
-    window.console.log(word[currentIndex]);
+    $("#messagebox").html(word[lessonIndex]);
 } // method
 function displayfinish() {
     'use strict';
-    $("#messagebox").html("Lesson Finished");
-    currentIndex = 0;
+    responsiveVoice.speak("This lesson is finished", "UK English Male", {
+        rate: 1.0
+    });
+    $("#messagebox").html("THIS LESSON IS FINISHED ");
 }
 
 function highlightword(word, start, end) {
@@ -41,52 +46,63 @@ function highlightword(word, start, end) {
     $("#messagebox").html(word);
 }
 
+function playSound() {
+    'use strict';
+    window.console.log("sound is playing");
+    gameOverSound.play();
+}
+
 function getLesson(selected_lesson) {
     'use strict';
-    // displayWord(selected_lesson);
+    lessonIndex = 0;
+    letterIndex = 0;
+    displayWord(selected_lesson);
+    stringlength = selected_lesson[lessonIndex].length;
     lesson = selected_lesson;
     lessonsize = selected_lesson.length;
-    stringlength = selected_lesson[currentIndex].length;
+    // window.console.log("string length is " + stringlength + " and lesson size is " + lessonsize);
 }
 $(document).on('keydown', function (ev) {
     'use strict';
     // displayWord(lesson);
-    if (ev.keyCode === 32) {
-        $('#start').fadeOut();
+    if (!is_it_finished) {
         displayWord(lesson);
+        is_it_finished = true;
     }
     else if (ev.keyCode === 13) { // press enter move to next word
-        if (currentIndex === lessonsize) {
+        if (lessonIndex === (lessonsize - 1)) {
             // is_it_finished = true;
-            responsiveVoice.speak("This lesson is finished", "UK English Male", {
-                rate: 1.0
-            });
+            lessonIndex = 0;
             displayfinish();
         }
-        if (letterIndex === stringlength) {
-            currentIndex += 1;
+        else if (letterIndex === stringlength) {
+            lessonIndex += 1;
             letterIndex = 0;
+            stringlength = lesson[lessonIndex].length;
             displayWord(lesson);
         }
         else {
-            // gameOverSound.play();
-            window.console.log("error");
+            playSound();
+            window.console.log("Lesson Index was " + lessonIndex + " and you should have pressed " + lesson[lessonIndex][letterIndex]);
+            //window.console.log(")
         }
     }
     else if (ev.keyCode >= 65 && ev.keyCode <= 90) {
         var letter = String.fromCharCode(ev.keyCode);
         letter = letter.toLowerCase();
-        if (letter === lesson[currentIndex][letterIndex]) {
-            window.console.log("Index is " + letterIndex);
-            responsiveVoice.speak(lesson[currentIndex][letterIndex], "UK English Female", {
+        if (letter === lesson[lessonIndex][letterIndex]) {
+            window.console.log("Letter Index is " + letterIndex + " and lesson Index was " + lessonIndex);
+            window.console.log("You pressed " + letter + " the key was supposed to be " + lesson[lessonIndex][letterIndex] + " letter index was " + letterIndex);
+            responsiveVoice.speak(lesson[lessonIndex][letterIndex], "UK English Female", {
                 rate: 1.5
             });
-            highlightword(lesson[currentIndex], 0, letterIndex);
+            highlightword(lesson[lessonIndex], 0, letterIndex);
             letterIndex += 1;
         }
         else {
-            gameOverSound.play();
+            playSound();
+            window.console.log("You pressed " + letter + " the key was supposed to be " + lesson[lessonIndex][letterIndex] + " letter index was " + letterIndex);
             window.console.log("error ");
         }
     }
-}); //document,keydown
+});
